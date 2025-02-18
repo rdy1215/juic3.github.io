c4dc9bda720c5c1200d8be07c253bf9f0f831bcd
fish360dogtv <fish360dogtv@gmail.com>
2025-02-19 04:05:59 +0800

    <feat> update use hash router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import App from './App';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
